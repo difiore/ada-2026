d3ad09253b8a54e7ef468a3def3ee906c04b1ce5
difiore <anthony.difiore@gmail.com>
2026-03-04 18:01:00 -0600

    commit

--- temp.R
+++ temp.R
@@ -24,10 +24,6 @@ p
 
 t.test(d$weight, mu = 7.2)
 
-
-
-
-
 f <- "https://raw.githubusercontent.com/difiore/ada-datasets/main/tbs-2006-2008-ranges.csv"
 d <- read_csv(f, col_names = TRUE)
 
@@ -45,33 +41,30 @@ s <- d |>
          se = sd/sqrt(nrow(d))
   )
 
-
 p <- ggplot(data = d, aes(x = sex, y = kernel95)) +
   geom_boxplot() +
   geom_jitter()
 
 p
 
-
 males <- d |> filter(sex == "M")
 females <- d |> filter(sex == "F")
 
 n_boot <- 10000
-boot_males <- (do(n_boot) *
+boot_males <- {do(n_boot) *
                  mean(sample(
                    males$kernel95,
                    nrow(males),
-                   replace = TRUE))) |>
+                   replace = TRUE))} |>
   pull(mean)
 
 histogram(boot_males)
 plotDist("norm", mean(boot_males), sd(boot_males), add=TRUE)
 
-
 # or
 
-boot_males <- (do(n_boot) *
-                 mean(resample(males$kernel95)))|>
+boot_males <- {do(n_boot) *
+                 mean(resample(males$kernel95))}|>
   pull(mean)
 m_males <- mean(boot_males)
 sd_males <- sd(boot_males)
@@ -79,22 +72,13 @@ sd_males <- sd(boot_males)
 histogram(boot_males)
 plotDist("norm", mean = m_males, sd = sd_males, add = TRUE)
 
-
-
-
-
 ci_boot_m <- quantile(boot_males, probs = c(0.025, 0.975))
-
 
 ci_norm_m <- m_males + qnorm(c(0.025, 0.975)) * sd_males
 
-
-
-
-boot_females <- (do(n_boot) *
-                   mean(resample(females$kernel95)))|>
+boot_females <- {do(n_boot) *
+                   mean(resample(females$kernel95))}|>
   pull(mean)
-
 
 
 m_females <- mean(boot_females)
@@ -106,15 +90,11 @@ plotDist("norm", mean = m_females, sd = sd_females, add = TRUE)
 ci_boot_f <- quantile(boot_females, probs = c(0.025, 0.975))
 ci_norm_f <- m_females + qnorm(c(0.025, 0.975)) * sd_females
 
-
 t_num <- mean(males$kernel95) - mean(females$kernel95) - 0
-
 
 t_denom <- sqrt(var(males$kernel95)/nrow(males) + var(females$kernel95)/nrow(females))
 
 (t <- t_num/t_denom)
-
-
 
 df_num <- (var(males$kernel95)/nrow(males) + var(females$kernel95)/nrow(females))^2
 
@@ -134,24 +114,10 @@ obs_diff <-
     summarize(diff = mean(kernel95[sex == "M"]) -
                      mean(kernel95[sex =="F"])) |>
   pull(diff)
-
 obs_diff
 
-perm_diff <- replicate(10000, {
-  d |>
-    mutate(sex = sample(sex)) |>
-    summarize(diff = mean(kernel95[sex == "M"]) -
-                     mean(kernel95[sex =="F"])) |>
-  pull(diff)
-  }
-)
-
-histogram(perm_diff)
-
-# or
 n_perm <- 10000
 perm_diff <- vector()
-
 for (i in 1:n_perm) {
   d |>
     mutate(sex = sample(sex)) |>
@@ -159,13 +125,31 @@ for (i in 1:n_perm) {
                 mean(kernel95[sex =="F"])) |>
     pull(diff) -> perm_diff[[i]]
 }
+histogram(perm_diff)
 
+# or...
+perm_diff <- {do(n_perm) *
+    d |>
+    mutate(sex=sample(sex)) |>
+    summarize(diff =	mean(kernel95[sex == "M"]) -
+                mean(kernel95[sex == "F"]))
+  } |>
+  pull(diff) # extract column of value as vector
+histogram(perm_diff)
+
+# or...
+perm_diff <- replicate(n_perm, {
+  d |>
+    mutate(sex=sample(sex)) |>
+    summarize(diff =	mean(kernel95[sex == "M"]) -
+                mean(kernel95[sex == "F"])) |>
+    pull(diff) # extract column of value as vector
+  })
 histogram(perm_diff)
 
 (p_val <- sum(abs(perm_diff) >= abs(obs_diff))/length(perm_diff))
 
 (p_val <- mean(abs(perm_diff) >= abs(obs_diff)))
-
 
 # infer package
 library(infer)
